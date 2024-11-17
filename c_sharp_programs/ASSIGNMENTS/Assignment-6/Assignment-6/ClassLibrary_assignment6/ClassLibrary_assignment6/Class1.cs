e4148c7cc7176b6ec6c52bec105eb496b41d8dfa
Tarunkumar Jami <tarunkumarj@infinite.com>
2024-11-17 22:43:16 +0530

    assignment6

---
+++ c_sharp_programs/ASSIGNMENTS/Assignment-6/Assignment-6/ClassLibrary_assignment6/ClassLibrary_assignment6/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_assignment6
+{
+    public class Class1
+    {
+        private const int totalfare = 500;
+        public void CalculateConcession(int age)
+        {
+
+            if (age <= 5)
+            {
+                Console.WriteLine("Little Champs - Free Ticket ");
+            }
+            else if (age > 60)
+            {
+                float concession = totalfare * 0.30f;
+                float finalfare = totalfare - concession;
+
+                Console.WriteLine("Senior citizen " + finalfare);
+            }
+            else
+            {
+                Console.WriteLine("Ticket Booked " + totalfare);
+            }
+        }
+    }
+}
---
+++ c_sharp_programs/ASSIGNMENTS/Assignment-6/Assignment-6/ClassLibrary_assignment6/ClassLibrary_assignment6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_assignment6
+{
+    public class Program
+    {
+        public const int totalfare = 500;
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the name:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Class1 c1 = new Class1();
+            c1.CalculateConcession(age);
+            Console.Read();
+
+
+        }
+
+    }
+}
